8c870d0cd0d2c15b0ad5250f1d0d26329715d48c
Kamola-13 <kam.mamatova@gmail.com>
2025-01-02 11:14:42 +0500

    feat: update with tasks 23-38

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -400,12 +400,12 @@ function toFixed(number, fractionDigits) {
  * 12345, 7    => '12345.00'
  * 12.345, 4   => '12.35'
  */
-function toPrecision(/* number, precision */) {
-  throw new Error('Not implemented');
+function toPrecision(number, precision) {
+  return number.toPrecision(precision);
 }
 
 /**
- * Returns the primitive value of a Number object.
+ * Returns the primitive value of a Number object. 24
  *
  * @param {Number} number
  * @return {number}
@@ -414,12 +414,12 @@ function toPrecision(/* number, precision */) {
  * new Number(5) => 5
  * Number(-5)    => -5
  */
-function getNumberValue(/* number */) {
-  throw new Error('Not implemented');
+function getNumberValue(number) {
+  return number.valueOf();
 }
 
 /**
- * Returns a boolean value indicating whether the parameter is a number or not.
+ * Returns a boolean value indicating whether the parameter is a number or not. 25
  *
  * @param {number} number
  * @return {boolean}
@@ -433,12 +433,12 @@ function getNumberValue(/* number */) {
  * 5        => true
  * '5'      => false
  */
-function isNumber(/* number */) {
-  throw new Error('Not implemented');
+function isNumber(number) {
+  return typeof number === 'number' && Number.isFinite(number);
 }
 
 /**
- * Returns a boolean value indicating whether a number is an integer or not.
+ * Returns a boolean value indicating whether a number is an integer or not. 26
  *
  * @param {number} number
  * @return {boolean}
@@ -448,12 +448,12 @@ function isNumber(/* number */) {
  * 5.1  => false
  * '5'  => false
  */
-function isInteger(/* number */) {
-  throw new Error('Not implemented');
+function isInteger(number) {
+  return Number.isInteger(number);
 }
 
 /**
- * Returns a floating point number or, if the number cannot be parsed from the argument, returns NaN.
+ * Returns a floating point number or, if the number cannot be parsed from the argument, returns NaN. 27
  *
  * @param {string} str
  * @return {number | NaN}
@@ -462,13 +462,13 @@ function isInteger(/* number */) {
  * '4.567abcdefgh' => 4.567
  * 'abcdefgh'      => NaN
  */
-function getFloatOnString(/* str */) {
-  throw new Error('Not implemented');
+function getFloatOnString(str) {
+  return Number.parseFloat(str);
 }
 
 /**
  * Returns an integer of the specified base or, if the number cannot be parsed
- * from the argument, returns NaN.
+ * from the argument, returns NaN. 28
  *
  * @param {string} str
  * @param {number} base
@@ -480,12 +480,12 @@ function getFloatOnString(/* str */) {
  * '1.234', 2           => 1
  * '10', 8              => 8
  */
-function getIntegerOnString(/* str, base */) {
-  throw new Error('Not implemented');
+function getIntegerOnString(str, base) {
+  return Number.parseInt(str, base);
 }
 
 /**
- * Returns whether a number is a safe integer.
+ * Returns whether a number is a safe integer. 29
  *
  * @param {number} number
  * @return {boolean}
@@ -495,12 +495,12 @@ function getIntegerOnString(/* str, base */) {
  * 3.5      => false
  * 2 ** 53  => false
  */
-function isSafeInteger(/* number */) {
-  throw new Error('Not implemented');
+function isSafeInteger(number) {
+  return Number.isSafeInteger(number);
 }
 
 /**
- * Returns the smallest integer less than or equal to a given number.
+ * Returns the smallest integer less than or equal to a given number. 30
  *
  * @param {number} number
  * @return {number}
@@ -509,12 +509,12 @@ function isSafeInteger(/* number */) {
  * 5.9  => 5
  * -5.1 => -6
  */
-function roundToSmallestInteger(/* number */) {
-  throw new Error('Not implemented');
+function roundToSmallestInteger(number) {
+  return Math.floor(number);
 }
 
 /**
- * Returns the largest integer greater than or equal to a given number.
+ * Returns the largest integer greater than or equal to a given number. 31
  *
  * @param {number} number
  * @return {number}
@@ -523,12 +523,12 @@ function roundToSmallestInteger(/* number */) {
  * 5.1  => 6
  * -5.9 => -5
  */
-function roundToLargestInteger(/* number */) {
-  throw new Error('Not implemented');
+function roundToLargestInteger(number) {
+  return Math.ceil(number);
 }
 
 /**
- * Returns the value of a number rounded to the nearest integer.
+ * Returns the value of a number rounded to the nearest integer. 32
  *
  * @param {number} number
  * @return {number}
@@ -538,12 +538,12 @@ function roundToLargestInteger(/* number */) {
  * 5.4  => 5
  * -5.5 => -5
  */
-function roundToNearestInteger(/* number */) {
-  throw new Error('Not implemented');
+function roundToNearestInteger(number) {
+  return Math.round(number);
 }
 
 /**
- * Returns the integer part of a number by removing any fractional digits.
+ * Returns the integer part of a number by removing any fractional digits. 33
  *
  * @param {number} number
  * @return {number}
@@ -553,12 +553,12 @@ function roundToNearestInteger(/* number */) {
  * 5.4  => 5
  * -5.5 => -5
  */
-function getIntegerPartNumber(/* number */) {
-  throw new Error('Not implemented');
+function getIntegerPartNumber(number) {
+  return Math.trunc(number);
 }
 
 /**
- * Returns the sum of numbers.
+ * Returns the sum of numbers. 34
  *
  * @param {number} x1
  * @param {number} x2
@@ -569,12 +569,13 @@ function getIntegerPartNumber(/* number */) {
  * 1, 2, 3       => 6
  * 0.1, 0.2, 0.3 => 0.6
  */
-function getSumOfNumbers(/* x1, x2, x3 */) {
-  throw new Error('Not implemented');
+function getSumOfNumbers(x1, x2, x3) {
+  const sum = x1 + x2 + x3;
+  return parseFloat(sum.toFixed(10));
 }
 
 /**
- * Returns the largest number.
+ * Returns the largest number. 35
  *
  * @param {number} firstNumber
  * @param {number} secondNumber
@@ -585,12 +586,12 @@ function getSumOfNumbers(/* x1, x2, x3 */) {
  * -5, -6 => -5
  * 0, 5   => 5
  */
-function getMaxNumber(/* firstNumber, secondNumber */) {
-  throw new Error('Not implemented');
+function getMaxNumber(firstNumber, secondNumber) {
+  return Math.max(firstNumber, secondNumber);
 }
 
 /**
- * Returns a random integer in the range from min to max.
+ * Returns a random integer in the range from min to max. 36
  *
  * @param {number} min
  * @param {number} max
@@ -601,12 +602,12 @@ function getMaxNumber(/* firstNumber, secondNumber */) {
  * -5, 0 => -5 | -4 | -3 | -2 | -1 | 0
  * -1, 1 => -1 | 0 | 1
  */
-function getRandomInteger(/* min, max */) {
-  throw new Error('Not implemented');
+function getRandomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 /**
- * Returns the length of the hypotenuse of a right triangle.
+ * Returns the length of the hypotenuse of a right triangle. 37
  *
  * @param {number} a
  * @param {number} b
@@ -615,12 +616,12 @@ function getRandomInteger(/* min, max */) {
  * @example:
  * 3, 4 => 5
  */
-function getHypotenuse(/* a, b */) {
-  throw new Error('Not implemented');
+function getHypotenuse(a, b) {
+  return Math.hypot(a, b);
 }
 
 /**
- * Returns count of odd numbers from zero to the resulting number.
+ * Returns count of odd numbers from zero to the resulting number. 38
  * The resulting number is taken into account.
  *
  * @param {number} number
@@ -632,8 +633,18 @@ function getHypotenuse(/* a, b */) {
  * 10 => 5
  * 15 => 8
  */
-function getCountOfOddNumbers(/* number */) {
-  throw new Error('Not implemented');
+function getCountOfOddNumbers(number) {
+  let count = 0;
+  if (number >= 0) {
+    for (let i = 0; i <= number; i += 1) {
+      if (i % 2 !== 0) count += 1;
+    }
+  } else {
+    for (let i = number; i <= 0; i += 1) {
+      if (i % 2 !== 0) count += 1;
+    }
+  }
+  return count;
 }
 
 module.exports = {
